4747c49517801fbc3bb83a51eadd45edd7e0b473
David Argueta <davidargueta503m@gmail.com>
2021-03-13 23:35:03 -0600

    dns_install beta full

--- dns_install.sh
+++ dns_install.sh
@@ -5,19 +5,16 @@ echo Verificar el nombre del servidor ---------
 hostnamectl
 
 echo ---------------------------------------------------
-sleep 3s
 
 echo Cambiando nombre del servidor ----------
 echo ----------------------
 read -p "Nuevo nombre del servidor: " nombre_server
-sleep 3s
+
 hostnamectl set-hostname $nombre_server
 echo --Nuevo nombre del server--
 hostnamectl
 echo " "
 echo ---------------------------------------------------
-
-sleep 3s
 
 echo Instalando herramientas de red ---------
 
@@ -25,40 +22,39 @@ sudo apt -y install net-tools
 
 echo ---------------------------------------------------
 
-sleep 3s
-
 echo Instalando servicio de DNS --------------
-sleep 2s
+
 echo Actualizando paquetes -------------------
+
 sudo apt update
-sleep 3s
+
 echo Instalando paquetes ---------------------
+
 sudo apt upgrade
-sleep 3s
 
 echo Instalando bind9 ------------------------
 
 sudo apt-get -y install bind9 bind9utils
-
-sleep 3s
 
 echo Verificar el contenido de la carpeta bind -
 
 sudo chmod -R 777 /etc/bind/
 
 sudo ls /etc/bind/
-echo " "
+
 echo ------------------------------------------------
-echo " "
+
 echo Configurando zona del DNS
-sleep 5s
+
 echo Ingrese la raiz de la zona:
 read -p "Raiz de la zona para DNS: " root_zona
-echo  " "
+
 echo Estas son las IP que tienen los adaptadores
+
 ifconfig
-echo " "
+
 echo Ingrese los primeros 3 octetos de la IP que desea configurar, por ejemplo: 0.168.192 para 192.168.0.X
+
 read -p "Fragamento de IP inversa: " ip_tres
 
 echo '
@@ -87,15 +83,15 @@ sudo cat /etc/bind/db.$root_zona
 sudo chmod -R 777 /etc/bind/
 
 echo ------------------------------------------------
-echo " "
+
 ifconfig
-echo " "
+
 read -p "Ingrese la IP que tendra el servidor DNS: " ip_server
 
 sudo sed -i "12,14d" /etc/bind/db.$root_zona
 
 sudo sed -i "s/localhost/$nombre_server/g" /etc/bind/db.$root_zona
-echo " "
+
 echo "
 @       IN  NS  $nombre_server.
 @       IN  A   $ip_server
@@ -108,14 +104,15 @@ www     IN  A   $ip_server
 echo ---------------------------------------------------
 sudo cat /etc/bind/db.$root_zona
 echo --------------------------------------------------
-echo " "
+
 sudo cp /etc/bind/db.127 /etc/bind/db.$ip_tres
-echo
+
 sudo sed -i "12,13d" /etc/bind/db.$ip_tres
 
 sudo sed -i "s/localhost/$root_zona/g" /etc/bind/db.$ip_tres
-echo " "
+
 sudo chmod -R 777 /etc/bind/
+
 echo "
 @       IN  NS   $nombre_server.
 @       IN  PTR  $root_zona
@@ -126,39 +123,51 @@ www     IN  A    $ip_server
 15      IN  PTR  $nombre_server
 15      IN  PTR  client.$root_zona
 " >> /etc/bind/db.$ip_tres
-echo " "
+
 sudo sudo cat /etc/bind/db.$ip_tres
+
 echo --------------------------------------------------
-echo " "
+
 echo Verificar la configuración del archivo named.conf ---
+
 sudo named-checkconf -z /etc/bind/named.conf
-echo 
+ 
 echo Verificar la configuración del archivo named.conf.local
+
 sudo named-checkconf -z /etc/bind/named.conf.local
-echo " "
+
 echo Verificar la configuración de la zona directa -----
+
 named-checkzone $root_zona /etc/bind/db.$root_zona
-echo " "
+
 echo Verificar la configuración de la zona inversa -----
+
 named-checkzone $root_zona /etc/bind/db.$ip_tres
-echo " "
+
+
 sudo cat /etc/default/named
-sudo shmod -R 777 /etc/default/
+
+sudo chmod -R 777 /etc/default/
+
 echo ---------------------------------------------------
+
 echo "Configurando servidor, solo para IPv4"
+
 sudo sed -i "6d" /etc/default/named
+
 echo 'OPTIONS="-u bind -4"' >> /etc/default/named
-sleep 3s
+
 sudo cat /etc/default/named
+
 echo ------------------------------------------------
 echo Iniciando el servidor------------------------------
+
 sudo systemctl start bind9
-sleep 5s
-echo " "
+
 echo Estado del servidor--------------------------------
+
 sudo systemctl status bind9
-echo " "
-sleep 5s
+
 echo         ----Fin---- 
 echo ---- © ServiCod 2021 -----------------------------
 
