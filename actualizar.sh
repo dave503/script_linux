0b16f927a51af319b4b64984d25f1be83bfcd295
William Alexander <william.al1379@gmail.com>
2021-03-17 03:47:17 -0600

    Add support to rhel and deb distro

--- actualizar.sh
+++ actualizar.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-echo Actualizando paquetes
-
+echo 'Actualizando paquetes'
 sleep 2s
-
-sudo apt-get update
-
-echo *********************************************
-
-echo Instalando paquetes 
-
-sleep 2s
-
-sudo apt-get -y upgrade
-
-sleep 2s
-
-echo Los paquetes han sido actualizados
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+if [[ -n $YUM_CMD ]]; then
+    sudo yum update
+    echo '*********************************************'
+    echo 'Instalando paquetes' 
+    sleep 2s
+    sudo yum -y upgrade
+    sleep 2s
+    echo 'Los paquetes han sido actualizados'
+elif [[ -n $APT_GET_CMD ]]; then
+    sudo apt-get update
+    echo '*********************************************'
+    echo 'Instalando paquetes'
+    sleep 2s
+    sudo apt-get -y upgrade
+    sleep 2s
+    echo 'Los paquetes han sido actualizados'
+else
+    echo "error can't update package cache"
+    exit 1;
+fi
