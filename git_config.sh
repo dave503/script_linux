92ac0b0f64b65cd1b739e931a834c200d4fe9a67
David Argueta <davidargueta503m@gmail.com>
2021-08-02 19:44:03 -0600

    Mod git_config

--- git_config.sh
+++ git_config.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo Actualizando repositorios
-
-sleep 2s
-
-sudo apt-get update
-
-sleep 2s
-
-echo Instalando Git
-
-sudo apt-get -y install git
-
-sleep 2s
-
 echo Configurando variables globales
 
 git config --global user.name "David Argueta"
